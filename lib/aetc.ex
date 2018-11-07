e7313f5ec67de08806c6108df4ce1e1efdd21fcc
johan-to <catchme@outside>
2018-11-07 15:53:41 +0000

    init

---
+++ lib/aetc.ex
@@ -0,0 +1,18 @@
+defmodule Aetc do
+  @moduledoc """
+  Documentation for Aetc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aetc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aetc_test.exs
@@ -0,0 +1,8 @@
+defmodule AetcTest do
+  use ExUnit.Case
+  doctest Aetc
+
+  test "greets the world" do
+    assert Aetc.hello() == :world
+  end
+end
